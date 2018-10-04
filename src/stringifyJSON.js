3717408b10a61b44d17c880d461c0494ea2dbff2
Chen Wilson <chenlwilson@gmail.com>
2018-10-03 18:39:00 -0700

    Complete Bare Minimum Requirements

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,43 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  //function, undefined
+
+
+  if (typeof obj === "function" || typeof obj === undefined || obj === null) {
+    return 'null';
+  }
+  if (typeof obj === "boolean" || typeof obj === "number") {
+    return obj.toString();
+  }
+  if (typeof obj === "string") { //string
+    return '"' + obj + '"';
+  }
+
+
+  if (Array.isArray(obj)) {
+    var strArr = '[';
+    for (var i = 0; i < obj.length; i++) {
+      strArr += stringifyJSON(obj[i]) + ',';
+    }
+
+    if (strArr.length > 1) {
+      return strArr = strArr.slice(0, strArr.length - 1) + ']';
+    } else {
+      return strArr + ']';
+    }
+  }
+  //obj
+  var strObj = '{';
+  for (var key in obj) {
+    if (obj[key] !== undefined && typeof obj[key] !== "function") {
+      strObj += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) + ',';
+    }
+  }
+  if (strObj.length > 1) {
+    return strObj = strObj.slice(0, strObj.length - 1) + '}';
+  } else {
+    return strObj + '}';
+  }
+
 };
