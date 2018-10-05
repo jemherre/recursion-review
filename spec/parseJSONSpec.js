55f2b834c4d3e48c929d65d7b8af6d346ffd2b35
Jessica Herrera <jherrera@fb.com>
2018-10-04 21:10:02 -0700

    worked on Advanced, attempted object recursion

--- spec/parseJSONSpec.js
+++ spec/parseJSONSpec.js
@@ -4,7 +4,9 @@ describe('parseJSON', function() {
   it('should match the result of calling JSON.parse', function() {
     parseableStrings.forEach(function(test) {
       var result = parseJSON(test);
+      //console.log("ours",JSON.stringify(result));
       var expected = JSON.parse(test);
+     // console.log("exp ",JSON.stringify(expected));
       var equality = _.isEqual(result, expected); // why can't we use `===` here?
       // Replace this line with an `expect` statement that tests
       // the behavior described by the `it` string
@@ -21,8 +23,8 @@ describe('parseJSON', function() {
       };
       // if you'd prefer, you can write your version of parseJSON
       // so that it passes this test instead of the one on line 21.
-      // expect(parseJSON(test)).to.equal(undefined);
-      expect(fn).to.throw(SyntaxError);
+      expect(parseJSON(test)).to.equal(undefined);
+      //expect(fn).to.throw(SyntaxError);
     });
   });
 
--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,7 +3,7 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  console.log("JSON >> "+json);
+  //console.log("JSON >> "+json);
 
     ////check first for "["
   if(json.charAt(0) === "["){
@@ -12,48 +12,54 @@ var parseJSON = function(json) {
     //split ,
     if(json.length == 2) return arr;
     var splitArr = json.slice(1,json.length-1).split(','); //get rid of []
-
     //[ "[1", "2", "3]" ]  >> [ "1", "2", "3]" ]
-    console.log("split "+splitArr, splitArr.length);
+  //  console.log("split "+splitArr, splitArr.length);
     for(var i =0; i < splitArr.length; i++){
-
       //start recursion for every element
-      arr.push(parseJSON(splitArr[i]));
+      arr.push(parseJSON(splitArr[i].trim()));
     }
-    console.log(arr);
+  //  console.log(arr);
     return arr;
   }
 
-  //check for objects
-  if(json.charAt(0) === "{"){
-    var obj= {};
-    //split ,
-    if(json.length == 2) return obj; //empty obj
-    var splitObj = json.slice(2,json.length-1).split(', "'); //get rid of {}
-    //{a:1,b:2} >> ["a:1","b:2"] >> [ ["a","1"],["b","2"] ]
-    console.log(splitObj);
-    for(var i =0; i < splitObj.length; i++){
-      console.log(splitObj[i])
-      //start recursion for every element
-      var pair = splitObj[i].split(': ');
-      console.log(pair);
-      var key = pair[0].slice(0,pair[0].length-1);
-      var val = parseJSON(pair[1]);
-      console.log(key,pair);
-      obj[key] = val;
-    }
-    console.log(JSON.stringify(obj));
-    return obj;
+//check for objects
+if(json.charAt(0) === "{"){
+  var obj= {};
+  //split ,
+  if(json.length == 2) return obj; //empty obj
+
+  var splitObj = json.slice(1,json.length-1).split(', "'); //get rid of {}
+  //{a:1,b:2} >> ["a:1","b:2"] >> [ ["a","1"],["b","2"] ]
+ // console.log("OBJ",splitObj,splitObj.length);
+  for(var i =0; i < splitObj.length; i++){
+  //  console.log(splitObj[i])
+    //start recursion for every element
+    var pair = splitObj[i].split(':');
+   // console.log("Pair",pair);
+    var key = parseJSON(pair[0].trim());
+    var val = parseJSON(pair[1].trim());
+  //  console.log(key,val);
+    obj[key] = val;
   }
+//  console.log(JSON.stringify(obj));
+  return obj;
+}
+
+  // //helper function to parse Obj
+  // var parseObj = function(arr){
+
+  // };
 
   //check for primitives
   if(isNaN(json)){ //string
-    console.log("json = "+json);
+
+  //  console.log("json = "+json);
     if(json === "true") return true;
     if(json === "false") return false;
     if(json === "null") return null;
-    if(json.charAt(0) === '"') return json.slice(1,json.length-1);//remove ""
-    if(json.charAt(0) === ' ') return parseJSON(json.slice(1));
+    if(json.charAt(0) === '"') return json.slice(1,json.length-1);//checks for strings
+    if(json.charAt(json.length-1) === '"') return json.slice(0,json.length-1);//checks for strings
+    if(json.charAt(0) === ' ' || json.charAt(json.length-1) === ' ' ) return parseJSON(json.trim());
     return json;
   } else {//its a number
     return Number(json);
